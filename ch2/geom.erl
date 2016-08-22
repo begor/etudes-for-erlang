ac5d01a5cf7e385cd5378936ee8ffbb425871190
Biriukov Egor <birykovegor@gmail.com>
2016-08-22 18:11:41 +0300

    2-2 - Docs.

--- ch2/geom.erl
+++ ch2/geom.erl
@@ -1,5 +1,8 @@
+%% @doc Functions for calculating geometrical formulas.
 -module (geom).
-
 -export ([area/2]).
 
+
+%% @doc Calculates the area of a rectangle, given
+%% its two sides.
 area(X, Y) -> X * Y.
