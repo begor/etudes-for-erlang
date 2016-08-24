3e61f0b62da870883c132f663d8ae74c06c25cef
Biriukov Egor <birykovegor@gmail.com>
2016-08-24 12:12:05 +0300

    4-2 - GCD.

---
+++ dijkstra.erl
@@ -0,0 +1,9 @@
+-module (dijkstra).
+-export ([gcd/2]).
+
+
+%% @doc Calculate GCD of two ints M and N
+%% implementig Dijkstra's algorithm.
+gcd(M, M) -> M;
+gcd(M, N) when M > N -> gcd(M - N, N);
+gcd(M, N) -> gcd(M, N-M).
