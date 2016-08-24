ba0d3461bc2ebeda32e518599992678fefc9ad6b
Biriukov Egor <birykovegor@gmail.com>
2016-08-24 12:25:36 +0300

    4-4 - Raise using TCO.

--- powers.erl
+++ powers.erl
@@ -4,5 +4,9 @@
 %% @doc Raises X to the Nth power.
 raise(_, 0) -> 1;
 raise(X, 1) -> X;
-raise(X, N) when N > 0 -> X * raise(X, N - 1);
+raise(X, N) when N > 0 -> raise(X, N, 1);
 raise(X, N) when N < 0 ->  1.0 / raise(X, -N).
+
+%% @doc Raises x to the Nth power using tail recursion.
+raise(_, 0, Acc) -> Acc;
+raise(X, N, Acc) -> raise(X, N - 1, X * Acc). 
