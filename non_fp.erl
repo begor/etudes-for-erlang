cd2ac98639e9d7cff805d1e69340108b83f74aae
Egor Biriukov <birykovegor@gmail.com>
2016-08-29 19:06:32 +0300

    6-5 - Random Numbers; Generating Lists of Lists

--- non_fp.erl
+++ non_fp.erl
@@ -1,22 +1,44 @@
 -module (non_fp).
--export ([generate_teeth/2]).
+-export ([generate_teeth/2, test_teeth/0]).
 
+%% @doc Generate a list of lists, six numbers per tooth, giving random
+%% pocket depths and string containint "T"s for present teeth and
+%% "F"s for missing ones.
+generate_teeth(Teeths, Prob) ->
+	random:seed(now()),
+	generate_teeth(Teeths, Prob, []).
 
-generate_teeth(GoodTeeth, Prob) -> generate_teeth(GoodTeeth, Prob, []).
+%% @doc Tail recursive generator of teeths.
+%% [0] denotes missing tooth.
+generate_teeth([], _, Acc) ->
+	lists:reverse(Acc);
+generate_teeth([$F|T], Prob, Acc) ->
+	generate_teeth(T, Prob, [[0]|Acc]);
+generate_teeth([$T|T], Prob, Acc) ->
+	generate_teeth(T, Prob, [generate_tooth(Prob)|Acc]).
 
-
-generate_teeth([], Prob, Acc) -> lists:reverse(Acc);
-generate_teeth([$F|T], Prob, Acc) -> generate_teeth(T, Prob, [0|Acc]);
-generate_teeth([$T|T], Prob, Acc) -> generate_teeth(T, Prob, [generate_tooth(Prob)|Acc]).
-
-
+%% @doc Generate one teeth's pockets.
 generate_tooth(Prob) ->
-	case random:uniform() < Prob of
-		true -> generate_tooth(2, 6, []);
-	    false -> generate_tooth(3, 6, [])
-	end.
+	Pockets = 6,
+	BaseDepth = case random:uniform() < Prob of
+								true -> 2;
+	  						false -> 3
+							end,
+	generate_tooth(BaseDepth, Pockets, []).
 
+%% @doc Generate one teeth's pockets given Base depth.
+generate_tooth(_, 0, Acc) ->
+	Acc;
+generate_tooth(Base, Left, Acc) ->
+	RandomDepth = Base + random:uniform(3) - 2,
+	generate_tooth(Base, Left - 1, [RandomDepth|Acc]).
 
-generate_tooth(_, 0, Acc) -> Acc;
-generate_tooth(Base, Left, Acc) -> 
-	generate_tooth(Base, Left - 1, [(Base + random:uniform(3) - 2)|Acc]).
+%% @doc Simple testing functions.
+test_teeth() ->
+	TList = "FTTTTFFTTTTTTTTFTTTTTTTTFFTTTTTT",
+	N = generate_teeth(TList, 0.5),
+	print_tooth(N).
+print_tooth([]) -> ok;
+print_tooth([H|T]) ->
+	io:format("~p~n", [H]),
+	print_tooth(T).
