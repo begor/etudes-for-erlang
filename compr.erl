332d49be4b270e89ceb2c5fe72fb40d21071e6e7
Egor Biriukov <birykovegor@gmail.com>
2016-08-29 19:23:26 +0300

    7-2: List Comprehensions and Pattern Matching

---
+++ compr.erl
@@ -0,0 +1,10 @@
+-module (compr).
+-export ([males_over_40/1, males_or_over_40/1]).
+
+%% @doc Returns names of males which age is over 40.
+males_over_40(People) ->
+  [Name || {Name, $M, Age} <- People, Age > 40].
+
+%% @doc Returns names of males or females which age is over 40.
+males_or_over_40(People) ->
+  [Name || {Name, Sex, Age} <- People, Age > 40 orelse Sex =:= $M].
