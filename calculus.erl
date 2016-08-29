ab18b435691810be9781fb4e1a5ebf7ae1492b77
Egor Biriukov <birykovegor@gmail.com>
2016-08-29 19:12:34 +0300

    7-1: Simple Higher Order Functions

---
+++ calculus.erl
@@ -0,0 +1,8 @@
+-module (calculus).
+-export ([deriviative/2]).
+
+%% @doc Calculates deriviative of a function F
+%% in the point X.
+deriviative(F, X) ->
+  Delta = 1.0e-10,
+  (F(X + Delta) - F(X)) / Delta.
