38220576c9dc97d57b35be68654974442ed12eaa
Egor Biriukov <birykovegor@gmail.com>
2016-08-30 18:26:51 +0300

    7-3: Using lists:foldl/3

--- stats.erl
+++ stats.erl
@@ -1,5 +1,5 @@
 -module (stats).
--export ([minimum/1, maximum/1, range/1]).
+-export([minimum/1, maximum/1, range/1, mean/1, stdv/1]).
 
 %% @doc Searches for a minimum element in a List.
 minimum([H|Tl]) -> minimum(H, Tl).
@@ -17,3 +17,12 @@ minimum(_, [H|Tl]) -> minimum(H, Tl).
 maximum(Max, []) -> Max;
 maximum(Max, [H|Tl]) when Max > H -> maximum(Max, Tl);
 maximum(_, [H|Tl]) -> maximum(H, Tl).
+
+%% @doc Calculates the mean for a list of Numbers.
+mean(Numbers) -> lists:foldl(fun(X, Y) -> X + Y end, 0, Numbers) / length(Numbers).
+
+%% @doc Calculates the standard deviation of Numbers.
+stdv(Numbers) ->
+  {Sum, SumSquares} = lists:foldl(fun(X, {Sum, SumSqr}) -> {Sum + X, SumSqr + X * X} end, {0, 0}, Numbers),
+  N = length(Numbers),
+  math:sqrt((N * SumSquares - Sum * Sum) / (N * (N - 1))).
